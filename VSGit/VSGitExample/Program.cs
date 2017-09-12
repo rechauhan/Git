65e2433296fade75015a52dc6d1857f400d25134
Rekha <rekhachauhan.72@gmail.com>
2017-09-12 12:19:35 +0530

    added new solution

---
+++ VSGit/VSGitExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSGitExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
